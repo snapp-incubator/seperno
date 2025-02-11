804ef10e27946f5b5f5199dd727571c5d2a51bae
sepehrsoh <59518359+sepehrsoh@users.noreply.github.com>
2025-02-11 20:00:12 +0330

    add linter

--- python/export.go
+++ python/export.go
@@ -3,9 +3,8 @@ package main
 /*
 #include <stdbool.h>
 */
+import "C"
 import (
-	"C"
-
 	"github.com/snapp-incubator/seperno/internal"
 	"github.com/snapp-incubator/seperno/pkg/options"
 )
@@ -13,9 +12,7 @@ import (
 // Exported normalization function using C.bool
 //
 //export NormalizeText
-func NormalizeText(input *C.char,
-	convertHalfSpace C.bool, combineSpace C.bool, removeOuterSpace C.bool, removeURL C.bool,
-	normalizePunctuations C.bool, endWithEOL C.bool, intToWord C.bool) *C.char {
+func NormalizeText(input *C.char, convertHalfSpace C.bool, combineSpace C.bool, removeOuterSpace C.bool, removeURL C.bool, normalizePunctuations C.bool, endWithEOL C.bool, intToWord C.bool) *C.char {
 	// Directly map C.bool values to Go's bool type for clarity
 	normOptions := options.NormalizerOptions{
 		ConvertHalfSpaceToSpace: bool(convertHalfSpace),

77ac016767761d9536a3335357f8202e5588776a
Mohammad Zareabidaki <mohammadzarea.bidaki@gmail.com>
2025-09-29 00:34:03 +0330

    fix the import of lfd in persian test

--- pkg/lfd/persian_number_test.go
+++ pkg/lfd/persian_number_test.go
@@ -282,7 +282,7 @@ func TestConvertWordsToIntFa(t *testing.T) {
 		},
 	}
 
-	detector := NewPersianNumberDetector()
+	detector := &PersianNumberDetector{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			results := detector.DetectNumbers(tt.input)
